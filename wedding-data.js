62e4911411b88d37e5b48458c5b887af11370052
ogutpride3-del <ogut.pride3@gmail.com>
2026-06-18 01:02:47 +0700

    Add files via upload

---
+++ wedding-data.js
@@ -0,0 +1,140 @@
+const weddingData = {
+  couple: {
+    bride: {
+      fullName: "ISMA LASARI",
+      nickName: "ISMA",
+      father: "CECEP SUPRIATNA",
+      mother: "NENENG RATNASARI",
+      instagram: "",
+      photo: "assets/images/bride/isma.jpg",
+    },
+
+    groom: {
+      fullName: "ADIT SETIAWAN",
+      nickName: "ADIT",
+      father: "APID MAHPUDIN",
+      mother: "ATI RUSMIATI",
+      instagram: "",
+      photo: "assets/images/groom/adit.jpg",
+    },
+  },
+
+  cover: {
+    title: "The Wedding Of",
+    brideName: "Isma",
+    groomName: "Adit",
+    date: "05 Juli 2026",
+    image: "assets/images/cover/cover.jpg",
+  },
+
+  opening: {
+    title: "Undangan Pernikahan",
+    subtitle:
+      "Dengan memohon rahmat dan ridha Allah Subhanahu Wa Ta'ala, kami bermaksud menyelenggarakan acara pernikahan putra-putri kami.",
+  },
+
+  quran: {
+    verse:
+      "Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang.",
+    source: "QS. Ar-Rum : 21",
+  },
+
+  event: {
+    akad: {
+      title: "Akad Nikah",
+      date: "2026-07-05T09:00:00",
+      day: "Minggu",
+      time: "09:00 WIB",
+      location: "KP Sindanglengo",
+      address:
+        "Jln Pajagalan KP Sindanglengo RT 02 / RW 01 Desa Tarajusari Kecamatan Banjaran",
+      maps:
+        "https://www.google.com/maps?q=-7.0415854,107.5914203&z=17&hl=en",
+    },
+
+    reception: {
+      title: "Resepsi",
+      date: "2026-07-05T11:00:00",
+      day: "Minggu, 05 Juli 2026",
+      time: "11:00 WIB - Selesai",
+      location: "Lapang Wakaf",
+      address: "Lapang Wakaf Sindang Lengo",
+      maps:
+        "https://www.google.com/maps?q=-7.0415854,107.5914203&z=17&hl=en",
+    },
+  },
+
+  countdown: {
+    targetDate: "2026-07-05T09:00:00",
+  },
+
+  loveStory: [
+    {
+      year: "2024",
+      title: "Awal Pertemuan",
+      description:
+        "Di tahun 2024 kami dipertemukan dan mulai menjalin komunikasi. Dari perkenalan sederhana, tumbuh rasa nyaman yang perlahan membawa kami saling mengenal lebih dalam.",
+    },
+
+    {
+      year: "26 Juni 2025",
+      title: "Pendekatan",
+      description:
+        "Kami memilih untuk bertemu dan menjalani hubungan dengan komunikasi yang baik. Saling bertukar cerita, berbagi perjalanan hidup, dan belajar memahami satu sama lain.",
+    },
+
+    {
+      year: "05 Juli 2026",
+      title: "Pernikahan",
+      description:
+        "Dengan izin Allah SWT kami memutuskan melangkah ke jenjang pernikahan. Memulai kehidupan baru sebagai pasangan suami istri, berharap keberkahan, kebahagiaan, dan cinta yang terus tumbuh hingga akhir hayat.",
+    },
+  ],
+
+  gallery: [
+    "assets/images/gallery/1.jpg",
+    "assets/images/gallery/2.jpg",
+    "assets/images/gallery/3.jpg",
+    "assets/images/gallery/4.jpg",
+    "assets/images/gallery/5.jpg",
+    "assets/images/gallery/6.jpg",
+    "assets/images/gallery/7.jpg",
+  ],
+
+  gift: {
+    bank: "BCA",
+    accountNumber: "8103103612",
+    accountName: "ADIT SETIAWAN",
+  },
+
+  music: {
+    title: "Anugerah Terindah",
+    file: "assets/audio/anugerah-terindah.mp3",
+  },
+
+  guestFamilies: {
+    brideSide: [
+      "Bpk Sholeh (Alm)",
+      "Bpk Udin Wahyudin (Alm)",
+      "Bpk Supriatna",
+      "Bpk Uli Mulia",
+      "Bpk Atep (Ketua RW 01)",
+      "Bpk Igit (Ketua RT 02)",
+    ],
+
+    groomSide: [
+      "Kel. Bpk Neb",
+      "Kel. Bpk Asrul",
+      "Kel. Bpk Tantan",
+      "Kel. Bpk Ajah Subarjah",
+    ],
+  },
+
+  footer: {
+    title: "Adit & Isma",
+    subtitle: "05 Juli 2026",
+    credit: "Designed & Developed By Kholid GA",
+  },
+};
+
+window.weddingData = weddingData;
